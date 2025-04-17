4bd2f18c70db68bb02d0b7f00d16afcb34f738a6
Oleg Shiryaev <oashiryaev@yandex.ru>
2025-04-17 13:12:30 +0300

    Страница Новости

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -5,6 +5,7 @@ app_name = "news"
 
 urlpatterns = [
     path("", views.news_list, name="news_list"),
+    path('load-more/', views.load_more_news, name='load_more_news'),
     path("widget/", views.news_widget, name="news_widget"),
     path("<slug:news_slug>/", views.news_detail, name="news_detail"),
 ]
--- apps/news/views.py
+++ apps/news/views.py
@@ -1,17 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core.paginator import Paginator
 from ..cities.models import City
 from .models import News
 
 
 def news_list(request, city_slug):
-    city = get_object_or_404(City, slug=city_slug, is_active=True)
-    news_list = News.objects.filter(cities=city)
+    city = get_object_or_404(City, slug=city_slug)
+    news_list = News.objects.filter(cities=city, is_published=True).order_by('-created_at')
+    paginator = Paginator(news_list, 6)  # Первые 6 новостей
+    page_obj = paginator.get_page(1)
+    
+    # Проверяем, есть ли ещё новости
+    has_more_news = paginator.count > 6
+    
+    return render(request, 'news/news_list.html', {
+        'news_list': page_obj,
+        'city': city,
+        'has_more_news': has_more_news
+    })
 
-    return render(
-        request, "news/news_list.html", {"city": city, "news_list": news_list}
-    )
+def load_more_news(request, city_slug):
+    if request.headers.get('X-Requested-With') != 'XMLHttpRequest':
+        return JsonResponse({'error': 'Invalid request'}, status=400)
+    
+    city = get_object_or_404(City, slug=city_slug)
+    page = int(request.GET.get('page', 2))
+    news_per_page = 3
+    news_list = News.objects.filter(cities=city, is_published=True).order_by('-created_at')
+    paginator = Paginator(news_list, news_per_page)
+    
+    try:
+        page_obj = paginator.page(page)
+    except:
+        return JsonResponse({'has_more_news': False, 'news': []})
+    
+    news_data = [{
+        'title': news.title,
+        'slug': news.slug,
+        'date': news.created_at.strftime('%d.%m.%Y'),
+        'content': (news.content[:100] + '...' if len(news.content) > 100 else news.content) if news.content else '',
+        'image': news.image.url if news.image else ''
+    } for news in page_obj]
+    
+    return JsonResponse({
+        'news': news_data,
+        'has_more_news': page_obj.has_next()
+    })
 
 
 def news_detail(request, city_slug, news_slug):
