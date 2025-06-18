14bd2b706b8d1f9ca1bdcdff4d72f33b7ebd26a8
Oleg Shiryaev <oashiryaev@yandex.ru>
2025-06-18 10:26:03 +0300

    Equipments page

--- config/settings.py
+++ config/settings.py
@@ -200,8 +200,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
-EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
+EMAIL_USE_SSL = int(env("EMAIL_USE_SSL", default=1))
 
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
@@ -274,24 +273,3 @@ LOCALITY_MIDDLEWARE_EXCLUDED_PATHS = [
 ]
 
 SITE_NAME = "АТК"
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-        "file": {
-            "class": "logging.FileHandler",
-            "filename": "debug.log",
-        },
-    },
-    "loggers": {
-        "": {
-            "handlers": ["console", "file"],
-            "level": "DEBUG",
-            "propagate": True,
-        },
-    },
-}
