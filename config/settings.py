41152e2485f0b1d95cb39964bcab6a29870a4f4d
Oleg Shiryaev <oashiryaev@yandex.ru>
2025-06-18 10:39:16 +0300

    Equipments page

--- config/settings.py
+++ config/settings.py
@@ -200,7 +200,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_USE_SSL = int(env("EMAIL_USE_SSL", default=1))
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
 
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
