c79f04543797f38a409ee7b09f81ebe2849f338c
Oleg Shiryaev <oashiryaev@yandex.ru>
2025-06-26 17:39:46 +0300

    Migrations

--- apps/news/migrations/0002_news_cities.py
+++ apps/news/migrations/0002_news_cities.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='news',
             name='cities',
-            field=models.ManyToManyField(related_name='news', to='cities.city'),
+            field=models.ManyToManyField(related_name='news', to='cities.locality'),
         ),
     ]
