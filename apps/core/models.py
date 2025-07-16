34a0dfa68df4e6b3c6e3f02caa2dbf57c102f635
olegshiryaev <oashiryaev@yandex.ru>
2025-07-16 15:49:42 +0300

    Callback modal

--- apps/core/models.py
+++ apps/core/models.py
@@ -353,7 +353,7 @@ class Feedback(models.Model):
 
     name = models.CharField(max_length=255, blank=True, null=True, verbose_name="Имя")
     phone = models.CharField(
-        max_length=20,
+        max_length=20, blank=True, null=True,
         verbose_name="Номер телефона",
     )
     content = models.TextField(verbose_name="Сообщение", blank=True, null=True)
