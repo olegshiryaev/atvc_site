0a54c46bb5bb2f3c82ba614281fe216d099a3ad1
olegshiryaev <oashiryaev@yandex.ru>
2025-08-18 14:48:25 +0300

    Sec

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -18,7 +18,7 @@ app_name = "core"
 
 urlpatterns = [
     path("", index, name="home"),
-    path('wfhlthch/getstatus', getstatus, name='getstatus'),
+    path("wfhlthch/getstatus/", getstatus, name='getstatus'),
     path("tariffs/<slug:slug>/", TariffDetailView.as_view(), name="tariff_detail"),
     path('tariff/<int:tariff_id>/equipment/', get_tariff_equipment, name='get_tariff_equipment'),
     path("about/", about_company, name="about"),
